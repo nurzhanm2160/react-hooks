a242b629e99f510318f80fc0adee4fcbe8463806
nurzhanm2160 <IFK-100321@umk.pl>
2021-11-30 06:10:37 +0100

    react custom hooks request, debounce, hover, scroll, input.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useCallback, useState} from 'react';
+import useInput from "./hooks/useInput";
+import Hover from "./components/Hover";
+import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
+import axios from "axios";
+import useRequest from "./hooks/useRequest";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [todos, isLoading, error] = useRequest(fetchTodos)
+
+    function fetchTodos() {
+        return axios.get(`https://jsonplaceholder.typicode.com/todos`)
+    }
+
+    if(isLoading) {
+        return <h1>Идет загрузка...</h1>
+    }
+
+    if(error) {
+        return <h1>Произошла ошибка при загрузку данных</h1>
+    }
+
+    return (
+        <div>
+            {todos && todos.map(todo =>
+                <div key={todo.id} style={{padding: 30, border: '2px solid black'}}>
+                    {todo.id}. {todo.title}
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default App;
